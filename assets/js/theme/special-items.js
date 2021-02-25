a0cd45e757b95cd3d286209bea7e7936f19dbdfb
Jamrod <myfavoritenoisemaker@gmail.com>
2021-02-25 11:21:40 -0700

    adding all to cart

--- assets/js/theme/special-items.js
+++ assets/js/theme/special-items.js
@@ -1,7 +1,65 @@
 import PageManager from "./page-manager";
 
 export default class Custom extends PageManager {
-    onReady() {
-        alert("Hello world!");
+    constructor(context) {
+        super(context);
     }
+    onReady() {
+        // alert("Hello world!");
+        this.newButtons()
+    }
+    newButtons() {
+        const addAll = document.querySelector("#add-all")
+        const removeAll = document.querySelector("#remove-all")
+        const products = jsContext.categoryProducts
+
+        addAll.addEventListener('click', addAllToCart)
+        removeAll.addEventListener('click', removeAllFromCart)
+
+        function addAllToCart() {
+            console.log("Adding...")
+            let items = {
+                "lineItems": []
+            }
+            products.forEach(product => {
+                items["lineItems"].push({
+                    "quantity": 1,
+                    "productId": product.id
+                })
+            })
+            createCart("/api/storefront/carts", items)
+                .then(data => console.log(JSON.stringify(data)))
+                .catch(err => console.log(err))
+        }
+
+        function removeAllFromCart() {
+            console.log("Removing...")
+        }
+
+        function createCart(url, cartItems) {
+            return fetch(url, {
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(cartItems),
+            })
+                .then(response => response.json())
+        }
+
+    }
+
+
 }
+function createCart(url, cartItems) {
+    return fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cartItems),
+    })
+        .then(response => response.json());
+};
