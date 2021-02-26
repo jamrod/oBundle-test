5ddb30406190fb2f263ebe4ad7ab9c607fd89ae6
Jamrod <myfavoritenoisemaker@gmail.com>
2021-02-25 17:10:39 -0700

    delete all fuctioning propperly, added customer name to top banner

--- assets/js/theme/special-items.js
+++ assets/js/theme/special-items.js
@@ -5,35 +5,61 @@ export default class Custom extends PageManager {
         super(context);
     }
     onReady() {
-        // alert("Hello world!");
         this.newButtons()
     }
     newButtons() {
         const addAll = document.querySelector("#add-all")
         const removeAll = document.querySelector("#remove-all")
         const products = jsContext.categoryProducts
+        var currentCart = jsContext.currentCart
+        getCart('/api/storefront/carts')
+            .then(data => checkCart(data))
+            .catch(err => console.log(err))
+
+        function checkCart(data) {
+            if (data.length > 0) {
+                currentCart = data.id
+                if (data[0].lineItems.physicalItems.length > 0) {
+                    removeAll.style.visibility = "visible"
+                }
+            } else {
+                currentCart = null
+            }
+        }
+
 
         addAll.addEventListener('click', addAllToCart)
         removeAll.addEventListener('click', removeAllFromCart)
 
         function addAllToCart() {
-            console.log("Adding...")
             let items = {
                 "lineItems": []
             }
+            let text = ""
             products.forEach(product => {
                 items["lineItems"].push({
                     "quantity": 1,
                     "productId": product.id
                 })
+                text += `Added ${product.name} to your cart\n`
             })
-            createCart("/api/storefront/carts", items)
-                .then(data => console.log(JSON.stringify(data)))
-                .catch(err => console.log(err))
+            if (currentCart) {
+                addCartItem('/api/storefront/carts/', currentCart, items)
+                    .then(data => afterPost(data, text, false))
+                    .catch(err => console.log(err))
+            } else {
+                createCart("/api/storefront/carts", items, false)
+                    .then(data => afterPost(data, text))
+                    .catch(err => console.log(err))
+            }
+
         }
 
         function removeAllFromCart() {
-            console.log("Removing...")
+            let url = '/api/storefront/carts/' + currentCart
+            deleteCart(url)
+                .then(data => afterPost(data, "Cart is now empty", true))
+                .catch(err => console.log(err))
         }
 
         function createCart(url, cartItems) {
@@ -48,18 +74,50 @@ export default class Custom extends PageManager {
                 .then(response => response.json())
         }
 
+        function addCartItem(url, cartId, cartItems) {
+            return fetch(url + cartId + '/items', {
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(cartItems),
+            })
+                .then(response => response.json())
+        }
+
+        function deleteCart(url) {
+            return fetch(url, {
+                method: "DELETE",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+        }
+
+        function afterPost(data, str, empty) {
+            const alertSpace = document.querySelector('#alert-space')
+            alertSpace.textContent = str
+            if (empty) {
+                currentCart = null
+                removeAll.style.visibility = "hidden"
+            } else {
+                currentCart = data.id
+                removeAll.style.visibility = "visible"
+            }
+        }
+
+        function getCart(url) {
+            return fetch(url, {
+                method: "GET",
+                credentials: "same-origin"
+            })
+                .then(response => response.json())
+        }
+
+
     }
 
 
 }
-function createCart(url, cartItems) {
-    return fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(cartItems),
-    })
-        .then(response => response.json());
-};
